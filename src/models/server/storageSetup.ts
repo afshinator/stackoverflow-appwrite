f339d7750aea3941fefa466ba6caffa1148e2f92
Afshin Mokhtari <afshinator@gmail.com>
2024-11-18 11:06:26 -0800

    Reformat to 2 spaces

--- src/models/server/storageSetup.ts
+++ src/models/server/storageSetup.ts
@@ -3,31 +3,31 @@ import { questionAttachmentBucket } from "../name";
 import { storage } from "./config";
 
 export default async function getOrCreateStorage() {
+  try {
+    await storage.getBucket(questionAttachmentBucket);
+    console.info("Storage Connected");
+  } catch (error) {
     try {
-        await storage.getBucket(questionAttachmentBucket);
-        console.info("Storage Connected");
-    } catch (error) {
-        try {
-            await storage.createBucket(
-                questionAttachmentBucket,
-                questionAttachmentBucket,
-                [
-                    Permission.create("users"),
-                    Permission.read("any"),
-                    Permission.read("users"),
-                    Permission.update("users"),
-                    Permission.delete("users"),
-                ],
-                false,
-                undefined,
-                undefined,
-                ["jpg", "png", "gif", "jpeg", "webp", "heic"]
-            );
+      await storage.createBucket(
+        questionAttachmentBucket,
+        questionAttachmentBucket,
+        [
+          Permission.create("users"),
+          Permission.read("any"),
+          Permission.read("users"),
+          Permission.update("users"),
+          Permission.delete("users"),
+        ],
+        false,
+        undefined,
+        undefined,
+        ["jpg", "png", "gif", "jpeg", "webp", "heic"]
+      );
 
-            console.info("Storage Created");
-            console.info("Storage Connected");
-        } catch (error) {
-            console.error("Error creating storage:", error);
-        }
+      console.info("Storage Created");
+      console.info("Storage Connected");
+    } catch (error) {
+      console.error("Error creating storage:", error);
     }
+  }
 }
